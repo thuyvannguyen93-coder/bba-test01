fa0db166c438c18215ec3c6313713ec4934758d0
Van <thuyvannguyen93@gmail.com>
2025-10-07 20:55:56 +0700

    baitap3

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,9 @@
+let playerName = 'Mario';
+let currentLives = '3';
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+const average = (level1 + level2 + level3) / 3;
+const remainder = (level1 + level2 + level3) % 3;
+console.log ("Giá trị trung bình coin của 3 level" , average);
+console.log ("Số coin dư khi chia cho 3" , remainder);
