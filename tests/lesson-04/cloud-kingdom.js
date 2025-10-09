95ab8201bcb04122031d9e60f1329927300813bc
Van <thuyvannguyen93@gmail.com>
2025-10-09 14:36:34 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,11 @@
+let powerUp = "mushroom";
+
+if (powerUp === "mushroom") {
+  console.log("Mario becomes Super!");
+} else if (powerUp === "flower") {
+  console.log("Mario can shoot fireballs!");
+} else if (powerUp === "star") {
+  console.log("Mario is invincible!");
+} else {
+  console.log("Unknown power-up!");
+}
