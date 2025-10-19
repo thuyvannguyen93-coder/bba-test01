432ce0dd9e9b58c977578c6017e07150d0c638e3
Van <thuyvannguyen93@gmail.com>
2025-10-19 08:02:53 +0700

    update final stage

--- tests/lesson-04/final-stage.js
+++ tests/lesson-04/final-stage.js
@@ -1,7 +1,11 @@
 let count = 0;
 for(let i = 1; i <= 100; i++) {
-    if (i % 17 === 0){
+    for (let j = i + 1; j <= 100; j++){
+    if ((i + j) % 17 === 0){
+        let sum = i + j;
         count ++;
+        console.log(`Cặp (${i}, ${j}) có tổng = ${sum}`);
     }
+ }
 }
-console.log("so chia het cho 17 la:" + count);
+console.log("cac cap so chia het cho 17 la:" + count);
